b5a9f5583981895cab6495f3f000b5a29c168874
AuroraPerego <aurora.perego@cern.ch>
2024-03-26 15:38:47 +0100

    implement streamCache

--- src/sycl/SYCLCore/ScopedContext.cc
+++ src/sycl/SYCLCore/ScopedContext.cc
@@ -4,34 +4,20 @@
 #include <sycl/sycl.hpp>
 
 #include "SYCLCore/ScopedContext.h"
-#include "chooseDevice.h"
+#include "SYCLCore/chooseDevice.h"
 #include "SYCLCore/verbose.h"
 
 namespace cms::sycltools {
+
   namespace impl {
 
-    void sycl_exception_handler(sycl::exception_list exceptions) {
-      std::ostringstream msg;
-      msg << "Caught asynchronous SYCL exception(s):";
-      for (auto const &exc_ptr : exceptions) {
-        try {
-          std::rethrow_exception(exc_ptr);
-        } catch (sycl::exception const &e) {
-          msg << '\n' << e.what();
-        }
-      }
-      throw std::runtime_error(msg.str());
-    }
-
     ScopedContextBase::ScopedContextBase(edm::StreamID streamID)
-        : stream_(chooseDevice(streamID, ::verbose), sycl_exception_handler, sycl::property::queue::in_order()) {}
+        : stream_(getStreamCache().get(chooseDevice(streamID, ::verbose))) {}
 
     ScopedContextBase::ScopedContextBase(ProductBase const &data)
-        : stream_(data.mayReuseStream()
-                      ? data.stream()
-                      : sycl::queue{data.device(), sycl_exception_handler, sycl::property::queue::in_order()}) {}
+        : stream_(data.mayReuseStream() ? data.streamPtr() : getStreamCache().get(data.device())) {}
 
-    ScopedContextBase::ScopedContextBase(sycl::queue stream) : stream_(std::move(stream)) {}
+    ScopedContextBase::ScopedContextBase(std::shared_ptr<sycl::queue> stream) : stream_(std::move(stream)) {}
 
     ////////////////////
 
@@ -46,23 +32,22 @@ namespace cms::sycltools {
         // Different streams, need to synchronize
         if (not available) {
           // Event not yet occurred, so need to add synchronization
-          // here. Sychronization is done by making the CUDA stream to
+          // here. Sychronization is done by making the SYCL stream to
           // wait for an event, so all subsequent work in the stream
           // will run only after the event has "occurred" (i.e. data
           // product became available).
-          dataEvent.wait();
+	      // FIXME_EVENT
+		  dataEvent.wait();
+          //stream().submit_barrier({dataEvent});
         }
       }
     }
 
     void ScopedContextHolderHelper::enqueueCallback(sycl::queue stream) {
-      // TODO: make truly asynchronous
-      try {
-        stream.wait_and_throw();
+      auto a = std::async([&]() {
+        stream.wait();
         waitingTaskHolder_.doneWaiting(nullptr);
-      } catch(...) {
-        waitingTaskHolder_.doneWaiting(std::current_exception());
-      }
+      });
     }
   }  // namespace impl
 
@@ -71,7 +56,7 @@ namespace cms::sycltools {
   ScopedContextAcquire::~ScopedContextAcquire() {
     holderHelper_.enqueueCallback(stream());
     if (contextState_) {
-      contextState_->set(stream());
+      contextState_->set(streamPtr());
     }
   }
 
